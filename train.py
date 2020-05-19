96280ce1087a71e32a90fef3a29e831635960fcb
jbutle55 <jbutle55@uwo.ca>
2020-05-19 10:10:25 -0600

    Increase number of epoch

--- train.py
+++ train.py
@@ -215,7 +215,7 @@ def run(train_dir, valid_dir, set_dir, model_dir):
                  terminate_on_nan]
 
     initial_epoch = 0
-    final_epoch = 30  # 120
+    final_epoch = 120  # 120
     steps_per_epoch = math.ceil(119/batch_size)  # ceil(num_samples/batch_size)
 
     # Training
